1f3493aa712251aebc8090e47b3a65816d94c512
alicechaplin <c_alice1@hotmail.com>
2021-03-03 13:49:58 +0100

    Initial Author Routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+
+// All Authors Route
 router.get('/', (req, res) =>{
     res.render('index')
 })
+
 
 module.exports = router
